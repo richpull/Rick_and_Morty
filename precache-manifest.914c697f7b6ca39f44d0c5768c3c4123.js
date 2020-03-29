0eb17002acd04199dc2f438937384ef430a752ab
user <user@A00000015.local>
2020-03-29 10:21:35 +0300

    Updates

---
+++ precache-manifest.914c697f7b6ca39f44d0c5768c3c4123.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d62046f111c2fb4490f4a8797ddba09",
+    "url": "/Rick_and_Morty/index.html"
+  },
+  {
+    "revision": "a3a3aded65e59ba45059",
+    "url": "/Rick_and_Morty/static/css/2.2fbdebf6.chunk.css"
+  },
+  {
+    "revision": "a3a3aded65e59ba45059",
+    "url": "/Rick_and_Morty/static/js/2.e9b2f5ac.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/Rick_and_Morty/static/js/2.e9b2f5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba667f86151cf6a34df2",
+    "url": "/Rick_and_Morty/static/js/main.3c52bdc4.chunk.js"
+  },
+  {
+    "revision": "a32ad8c0776d0e88621a",
+    "url": "/Rick_and_Morty/static/js/runtime-main.f4ed8d3f.js"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/Rick_and_Morty/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/Rick_and_Morty/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/Rick_and_Morty/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/Rick_and_Morty/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/Rick_and_Morty/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rm/precache-manifest.6cc067bb2270250bef6882dd0f6d591a.js"
+  "/Rick_and_Morty/precache-manifest.914c697f7b6ca39f44d0c5768c3c4123.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rm/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rick_and_Morty/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
